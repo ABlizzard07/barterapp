1382ae13aa22d63f7282c000d505bca755ef98dc
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2021-02-20 17:53:17 -0600

    Add files via upload

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,117 +1,327 @@
-import React,{Component} from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity, Alert, Image } from 'react-native';
-import db from '../config.js';
-import firebase from 'firebase'
+import React,{Component}from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    Modal,
+    KeyboardAvoidingView,
+    StyleSheet,
+    TouchableOpacity,
+    Alert,
+    ScrollView} from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+
+
 
 export default class WelcomeScreen extends Component{
   constructor(){
     super();
-    this.state = {
-      emailId: '',
-      password: ''
+    this.state={
+      emailId:'',
+      password:'',
+      firstName:'',
+      lastName:'',
+      address:'',
+      contact:'',
+      confirmPassword:'',
+      isModalVisible:'false'
     }
   }
 
-  userLogin = async (email, password) => {
-    firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
-      return alert('Successfully logged in!')   
-    })
+  userSignUp = (emailId, password,confirmPassword) =>{
+   if(password !== confirmPassword){
+       return alert("Password does not match.")
+   }else{
+     firebase.auth().createUserWithEmailAndPassword(emailId, password)
+     .then(()=>{
+       db.collection('Users').add({
+         first_name:this.state.firstName,
+         last_name:this.state.lastName,
+         contact:this.state.contact,
+         email_id:this.state.emailId,
+         address:this.state.address
+       })
+       return alert(
+            'User Added Successfully',
+            '',
+            [
+              {text: 'OK', onPress: () => this.setState({"isModalVisible" : false})},
+            ]
+        );
+     })
+     .catch((error)=> {
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       return alert(errorMessage)
+     });
+   }
+ }
 
-    .catch((error) => {
-      var theerror = error.code;
-      var errorMessage = theerror.message;
+userLogin = (emailId, password)=>{
+   firebase.auth().signInWithEmailAndPassword(emailId, password)
+  
+   .catch((error)=> {
+     var errorCode = error.code;
+     var errorMessage = error.message;
+     return alert(errorMessage)
+   })
+ }
 
-      return alert(errorMessage)
-      
-    })
-  }
-
-  userSignUp = async (email, password) => {
-    firebase.auth().createUserWithEmailAndPassword(email, password).then((response) => {
-      return alert('Welcome to the Barter App!');
-    })
-      .catch((error) => {
-        var theerror = error.code;
-        var errorMessage = theerror.message;
-
-        return alert(errorMessage)
-      })
-  }
-
+showModal = ()=>{
+  return(
+  <Modal
+    animationType="fade"
+    transparent={true}
+    visible={this.state.isModalVisible}
+    >
+    <View style={styles.modalContainer}>
+      <ScrollView style={{width:'100%'}}>
+        <KeyboardAvoidingView style={styles.KeyboardAvoidingView}>
+        <Text
+          style={styles.modalTitle}
+          >Sign Up</Text>
+        <TextInput
+          style={styles.formInput}
+          placeholder ={"First Name"}
+          maxLength ={12}
+          onChangeText={(text)=>{
+            this.setState({
+              firstName: text
+            })
+          }}
+        />
+        <TextInput
+          style={styles.formInput}
+          placeholder ={"Last Name"}
+          maxLength ={12}
+          onChangeText={(text)=>{
+            this.setState({
+              lastName: text
+            })
+          }}
+        />
+        <TextInput
+          style={styles.formInput}
+          placeholder ={"Contact"}
+          maxLength ={10}
+          keyboardType={'numeric'}
+          onChangeText={(text)=>{
+            this.setState({
+              contact: text
+            })
+          }}
+        />
+        <TextInput
+          style={styles.formInput}
+          placeholder ={"Address"}
+          multiline = {true}
+          onChangeText={(text)=>{
+            this.setState({
+              address: text
+            })
+          }}
+        />
+        <TextInput
+          style={styles.formInput}
+          placeholder ={"Email"}
+          keyboardType ={'email-address'}
+          onChangeText={(text)=>{
+            this.setState({
+              emailId: text
+            })
+          }}
+        /><TextInput
+          style={styles.formInput}
+          placeholder ={"Password"}
+          secureTextEntry = {true}
+          onChangeText={(text)=>{
+            this.setState({
+              password: text
+            })
+          }}
+        /><TextInput
+          style={styles.formInput}
+          placeholder ={"Confirm Password"}
+          secureTextEntry = {true}
+          onChangeText={(text)=>{
+            this.setState({
+              confirmPassword: text
+            })
+          }}
+        />
+        <View>
+          <TouchableOpacity
+            style={styles.registerButton}
+            onPress={()=>
+              this.userSignUp(this.state.emailId, this.state.password, this.state.confirmPassword)
+            }
+          >
+          <Text style={styles.buttonText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={styles.cancelButton}
+            onPress={()=>this.setState({"isModalVisible":false})}
+          >
+          <Text style={{color:'#ff5722'}}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </View>
+  </Modal>
+)
+}
   render(){
-    return (
-      <View style = {styles.container}>
-       <Text style = {styles.title}>Bartering App</Text>
+    return(
+      <View style={styles.container}>
+       
+          {
+            this.showModal()
+          }
 
-       <TextInput style = {styles.input} onChangeText = {(text) => {
-         this.setState({
-           emailId: text
-         })
-       }} value = {this.state.emailId} placeholder = 'Email' placeholderTextColor = 'blue' keyboardType = 'emailAddress'></TextInput>
+        <View style={{justifyContent:'center', alignItems:'center'}}>
+        
+          <Text style={styles.title}>Barter App</Text>
+        </View>
+        <View>
+            <TextInput
+            style={styles.loginBox}
+            placeholder="Email"
+            keyboardType ='email-address'
+            onChangeText={(text)=>{
+              this.setState({
+                emailId: text
+              })
+            }}
+          />
+          <TextInput
+          style={styles.loginBox}
+          secureTextEntry = {true}
+          placeholder="Password"
+          onChangeText={(text)=>{
+            this.setState({
+              password: text
+            })
+          }}
+        />
+        <TouchableOpacity
+           style={[styles.button,{marginBottom:20, marginTop:20}]}
+           onPress = {()=>{
+             this.userLogin(this.state.emailId, this.state.password)
+           }}
+           >
+           <Text style={styles.buttonText}>Login</Text>
+         </TouchableOpacity>
 
-       <TextInput style = {styles.input} onChangeText = {(text) => {
-         this.setState({
-           password: text
-         })
-       }} value = {this.state.password} placeholder = 'Password' secureTextEntry = {true} placeholderTextColor = '#000044' keyboardType = 'emailAddress'></TextInput>
-
-       <TouchableOpacity style = {styles.submit} onPress = {this.userSignUp(this.state.emailId, this.state.password)}>
-         <Text style = {styles.buttonText}>Sign Up</Text>
-       </TouchableOpacity>
-
-       <TouchableOpacity style = {styles.submit} onPress = {this.userLogin(this.state.emailId, this.state.password)}>
-         <Text style = {styles.buttonText}>Log In</Text>
-       </TouchableOpacity>
+         <TouchableOpacity
+           style={styles.button}
+           onPress={()=>this.setState({ isModalVisible:true})}
+           >
+           <Text style={styles.buttonText}>Sign Up</Text>
+         </TouchableOpacity>
       </View>
+    </View>
     )
   }
-
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#F8BE85',
-        alignItems: 'center',
-        justifyContent: 'center'
-      },
-      title :{
-        fontSize:65,
-        fontWeight:'300',
-        paddingBottom:30,
-        color : '#BB0000'
-      },
-      input:{
-        width: '70%',
-        height: 40,
-        borderBottomWidth: 2,
-        borderColor : '#CC11DD',
-        fontSize: 20,
-        marginTop:15,
-        padding:10,
-        alignSelf: 'center'
-      },
-      button:{
-        width:200,
-        height:80,
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius:25,
-        backgroundColor:"#00DD00",
-        shadowColor: "#000",
-        shadowOffset: {
-           width: 0,
-           height: 10,
-        },
-        shadowOpacity: 0.5,
-        shadowRadius: 10,
-        elevation: 15,
-        padding: 10
-      },
-      buttonText:{
-        color:'#004400',
-        fontWeight:'200',
-        fontSize:20
-      }
-     
+  container:{
+   flex:1,
+   backgroundColor:'peach',
+   alignItems: 'center',
+   justifyContent: 'center'
+ },
+ title :{
+   fontSize:60,
+   fontWeight:'300',
+   paddingBottom:30,
+   color : '#ff3d00'
+ },
+ loginBox:{
+   width: 280,
+   height: 40,
+   borderBottomWidth: 1.5,
+   borderColor : 'red',
+   fontSize: 20,
+   margin:10,
+   paddingLeft:10
+ },
+ KeyboardAvoidingView:{
+   flex:1,
+   justifyContent:'center',
+   alignItems:'center'
+ },
+ modalTitle :{
+   justifyContent:'center',
+   alignSelf:'center',
+   fontSize:30,
+   color:'maroon',
+   margin:50
+ },
+ modalContainer:{
+   flex:1,
+   borderRadius:20,
+   justifyContent:'center',
+   alignItems:'center',
+   backgroundColor:"#ffff",
+   marginRight:30,
+   marginLeft : 30,
+   marginTop:80,
+   marginBottom:80,
+ },
+ formInput:{
+   width:"70%",
+   height:30,
+   alignSelf:'center',
+   borderColor:'#ff4402',
+   borderRadius:10,
+   borderWidth:1,
+   marginTop:20,
+   padding:10
+ },
+ registerButton:{
+   width:200,
+   height:40,
+   alignItems:'center',
+   justifyContent:'center',
+   borderWidth:1,
+   borderRadius:10,
+   marginTop:30
+ },
+ registerButtonText:{
+   color:'#ff5722',
+   fontSize:15,
+   fontWeight:'bold'
+ },
+ cancelButton:{
+   width:180,
+   height:30,
+   justifyContent:'center',
+   alignItems:'center',
+   marginTop:5,
+ },
+
+ button:{
+   width:320,
+   height:50,
+   justifyContent:'center',
+   alignItems:'center',
+   borderRadius:25,
+   backgroundColor:"#ff9800",
+   shadowOffset: {
+      width: 0,
+      height: 8,
+   },
+   shadowOpacity: 0.30,
+   padding: 10
+ },
+ buttonText:{
+   color:'red',
+   fontWeight:'200',
+   fontSize:20
+ }
 })
