1f2021aa7101b6f2e90032a5bb57ab29a9d4ff6d
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2021-02-27 17:46:16 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import ExchangeScreen from '../screens/ExchangeScreen'
 import HomeScreen from '../screens/HomeScreen';
-
-
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import Sidebar from './assets/Sidebar'
+import SettingsScreen from './assets/SettingsScreen'
 
 export const AppTabNavigator = createBottomTabNavigator({
   Home : {
@@ -19,3 +20,18 @@ export const AppTabNavigator = createBottomTabNavigator({
     }
   }
 });
+
+const AppDrawNavigator = createDrawerNavigator({
+   Home: {
+     screen: AppTabNavigator
+   },
+   Settings: {
+     screen: SettingsScreen
+   }
+  },
+  {
+    contentComponent: Sidebar
+  },
+  {
+    initialRouteName: 'Home'
+  })
